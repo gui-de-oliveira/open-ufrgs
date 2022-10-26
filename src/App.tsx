9b6d07c32f327dbcaccea229fce59f0212a488f7
GuiOliveira98 <guil.oliveira98@gmail.com>
2022-10-26 20:34:14 -0300

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -7,43 +7,51 @@ function App() {
   const [user, setUser] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [classes, setClasses] = useState<string | null>(null);
+  const x = async () => {
+    const response = await axios.post(`${BASE_URL}/login`, {
+      user,
+      password,
+    });
+
+    const { sessionId } = response.data;
+
+    const responseGet = await axios.get(`${BASE_URL}/available-classes`, {
+      headers: {
+        authorization: sessionId,
+      },
+    });
+
+    setClasses(JSON.stringify(responseGet.data));
+  };
 
   if (classes === null) {
-    return (
-      <div>
-        <input value={user} onChange={(ev) => setUser(ev.target.value)} />
-        <input
-          value={password}
-          onChange={(ev) => setPassword(ev.target.value)}
-        />
-        <button
-          onClick={async () => {
-            const response = await axios.post(`${BASE_URL}/login`, {
-              user,
-              password,
-            });
-
-            const { sessionId } = response.data;
-
-            const responseGet = await axios.get(
-              `${BASE_URL}/available-classes`,
-              {
-                headers: {
-                  authorization: sessionId,
-                },
-              }
-            );
-
-            setClasses(JSON.stringify(responseGet.data));
-          }}
-        >
-          SEND
-        </button>
-      </div>
-    );
+    return <LoginScreen />;
   }
 
   return <div>{classes}</div>;
+}
+
+function LoginScreen() {
+  return (
+    <div className="container">
+      <h1>Open UFRGS</h1>
+      <br />
+      <form>
+        <div className="mb-3">
+          <label className="form-label">Número do cartão</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Senha do portal</label>
+          <input type="password" className="form-control" />
+        </div>
+
+        <button type="button" className="btn btn-primary">
+          Entrar
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
